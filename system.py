5678514fce09be3234a0907e9bc004a683d39839
Juan Delgado <juan@ustwo.co.uk>
2013-01-31 12:21:44 +0000

    Fixed bug when trying to access Windows APPDATA env var while on non-Windows machines.

--- system.py
+++ system.py
@@ -12,8 +12,6 @@ class system:
 		file.write(content)
 		file.close()
 
-	save = Callable.Callable(save)
-
 	def get_app_data_folder():
 
 		app_data_folder = system.get_system_app_data_folder() + "/.sonos_cli"
@@ -23,15 +21,24 @@ class system:
 
 		return app_data_folder
 
-	get_app_data_folder = Callable.Callable(get_app_data_folder)
-
 	def get_system_app_data_folder():
 
 		# OS-specific paths taken from here:
 		# http://stackoverflow.com/questions/57019/where-should-cross-platform-apps-keep-their-data
 		return {
-			"Windows": os.environ["APPDATA"],
+			"Windows": system.get_system_env("APPDATA"),
 			"Darwin": os.path.expanduser("~") + "/Library/Application Support",
 			"Linux": os.path.expanduser("~")
 		}.get(platform.system(), False)
+
+	def get_system_env(env):
+		
+		try:
+			return os.environ[env]
+		except KeyError:
+			return False
+
+	save = Callable.Callable(save)
+	get_app_data_folder = Callable.Callable(get_app_data_folder)
 	get_system_app_data_folder = Callable.Callable(get_system_app_data_folder)
+	get_system_env = Callable.Callable(get_system_env)
